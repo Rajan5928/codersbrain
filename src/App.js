4648dea5f4606e4983201a08968828b565343cac
Rajan5928 <prasoonkumar887@gmail.com>
2020-06-11 21:35:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import './App.css';
-
+import './index.css';
+import Home from './components/pages/Home';
+import Admin from './components/pages/Admin';
+import NotFound from './components/pages/NotFound';
+import Navbar from './components/layout/Navbar';
+import AddUser from './components/users/AddUser';
+import EditUser from './components/users/EditUser';
+import Userss from './components/users/Userss';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/admin" component={Admin} />
+          <Route exact path="/users/add" component={AddUser} />
+          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route exact path="/users/:id" component={Userss} />
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="container">
+            <div className="py-4">
+                <h1>Check out the Admin Page</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;

5b27e31d2908f2d84918e2c741b954c78456a498
Rajan5928 <prasoonkumar887@gmail.com>
2020-06-12 08:19:55 +0530

    fifth commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,8 +4,8 @@ const Home = () => {
     return(
         <div className="container">
             <div className="py-4">
-                <h1>Please run the command npm run json-server to populate data in Admin page</h1>
-                <h2>After executing the above command refresh the app and goto Admin page</h2>
+                <h1>Please run the command [npm run start:dev] instead of [npm start]</h1>
+                <h2>Wait for a while</h2>
             </div>
         </div>
     )

40f6553f51d79e8e4f6eaefe3f97ad7c4720f8fa
Rajan5928 <prasoonkumar887@gmail.com>
2020-06-24 17:17:18 +0530

    changed logo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
             <div className="container">
-                <a className="navbar-brand" href="/">AMTS</a>
+                <a className="navbar-brand" href="/">CareerLabs</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>

3ff0ebcc84382605255769a644a7d275fdc5613b
Rajan5928 <prasoonkumar887@gmail.com>
2020-06-24 17:11:24 +0530

    changed the database

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,9 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import { CSVLink} from "react-csv";
 import {Link, NavLink} from 'react-router-dom';
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
 const Navbar = () => {
     const [users, setUser] = useState([]);
     useEffect(() => {
@@ -12,10 +15,36 @@ const Navbar = () => {
         setUser(result.data);
     };
     const k= JSON.parse(JSON.stringify(users));
+
+    const exportPDF = () => {
+        const unit = "pt";
+        const size = "A4"; // Use A1, A2, A3 or A4
+        const orientation = "portrait"; // portrait or landscape
+
+        const marginLeft = 40;
+        const doc = new jsPDF(orientation, unit, size);
+
+        doc.setFontSize(15);
+
+        const title = "INVOICE";
+        const headers = [["Sl.No", "Brand Friend Name", "Business Name", "Transaction ID"]];
+
+        const data = k.map((elt, index) => [index + 1, elt.name, elt.username, elt.email]);
+
+        let content = {
+            startY: 50,
+            head: headers,
+            body: data
+        };
+
+        doc.text(title, marginLeft, 40);
+        doc.autoTable(content);
+        doc.save("report.pdf")
+    }
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
             <div className="container">
-                <a className="navbar-brand" href="/">CODERSBRAIN</a>
+                <a className="navbar-brand" href="/">AMTS</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -30,9 +59,8 @@ const Navbar = () => {
                         </li>
                     </ul>
                 </div>
-
-                <CSVLink data={k} className="btn btn-outline-light mr-2" >Export Data</CSVLink>
-                <Link className="btn btn-outline-light" to="/users/add">Add User</Link>
+                <CSVLink data={k} className="btn btn-outline-light mr-2" onClick={() => exportPDF()}>Export Data</CSVLink>
+                    {/* <Link className="btn btn-outline-light" to="/users/add">Create Course</Link> */}
             </div>
             
         </nav>
--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 
-
 const Admin = () => {
     const [users, setUser] = useState([]);
     const [search, setSearch] = useState("");
@@ -11,30 +10,40 @@ const Admin = () => {
     }, []);
     const loadUsers= async () => {
         const result = await axios.get("http://localhost:3003/users");
-        setUser(result.data.reverse());
+        setUser(result.data);
     };
     const deleteUser= async id => {
         await axios.delete(`http://localhost:3003/users/${id}`);
         loadUsers();
     }
+    
     let filteredUsers = users.filter(
         (user) => {
-            return user.email.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+            return user.Provider.toLowerCase().indexOf(search.toLowerCase()) !== -1;
         }
     );
+    
     return (
-        <div className="container">
+        <div className="container position-relative">
             <div className="py-4">
-                <h1>Admin Page</h1>
-                <input className="my-3 shadow form-control" type="text" placeholder="Search Bar" 
+                <div className="d-flex justify-content-between">
+                    <h1>Admin Page</h1>
+                </div>
+                <input className="my-3 shadow form-control" type="text" placeholder="Search By Provider" 
                     onChange={e => setSearch(e.target.value)}/>
                 <table className="table border shadow">
                     <thead className="thead-dark">
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">User Name</th>
-                            <th scope="col">Email</th>
+                            <th scope="col">Course ID</th>
+                            <th scope="col">Course Name</th>
+                            <th scope="col">Provider</th>
+                            <th scope="col">Universities/Institutions</th>
+                            <th scope="col">Parent Subject</th>
+                            <th scope="col">Child Subject</th>
+                            <th scope="col">URL</th>
+                            <th scope="col">Next Session Date</th>
+                            <th scope="col">Length</th>
+                            <th scope="col">Video URL</th>
                             <th>Action</th>
                         </tr>
                     </thead>
@@ -43,12 +52,18 @@ const Admin = () => {
                             filteredUsers.map((user,index)=>(
                                 <tr key={index}>
                                     <th scope="row">{index + 1}</th>
-                                    <td>{user.name}</td>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
+                                    <td>{user.CourseName}</td>
+                                    <td>{user.Provider}</td>
+                                    <td>{user.Universities}</td>
+                                    <td>{user.ParentSubject}</td>
+                                    <td>{user.ChildSubject}</td>
+                                    <td><a href={user.Url} target="_blank">{user.Url}</a></td>
+                                    <td>{user.NextSessionDate}</td>
+                                    <td>{user.Length}</td>
+                                    <td><a href={user.VideoUrl} target="_blank">{user.VideoUrl}</a></td>
                                     <td>
-                                        <Link className="btn btn-primary mr-2" to={`users/${user.id}`}>View</Link>
-                                        <Link className="btn btn-outline-primary mr-2" to={`users/edit/${user.id}`}>Edit</Link>
+                                        {/* <Link className="btn btn-primary mr-2" to={`users/${user.id}`}>View</Link> */}
+                                        {/* <Link className="btn btn-outline-primary mr-2" to={`users/edit/${user.id}`}>Edit</Link> */}
                                         <button className="btn btn-danger" onClick={()=>deleteUser(user.id)}>Delete</button>
                                     </td>
                                 </tr>
